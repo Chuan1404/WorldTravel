a5aec122e478381c3fb979d75dabb94eb97f875a
Anchu <chuan14402@gmail.com>
2024-01-11 20:57:38 +0700

    fix UI bug

--- src/main/java/com/worldstory/travel/models/Role.java
+++ src/main/java/com/worldstory/travel/models/Role.java
@@ -25,4 +25,9 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return name.name();
     }
+
+    @Override
+    public String toString() {
+        return name.name();
+    }
 }
